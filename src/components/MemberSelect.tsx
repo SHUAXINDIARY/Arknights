6970882fd70fc7a9b5b97268e9523c45e305a3a3
biantao <tao.bian@js.design>
2024-11-21 15:02:45 +0800

    feat: 干员 & 活动支持搜索

--- src/components/MemberSelect.tsx
+++ src/components/MemberSelect.tsx
@@ -1,4 +1,10 @@
-import { Select, SelectItem } from "@nextui-org/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  // Select,
+  // SelectItem,
+  Autocomplete,
+  AutocompleteItem,
+} from "@nextui-org/react";
 import { memberNameAvatarMap } from "../data/NameAvatar";
 import LazyLoadAvatar from "./LazyLoadAvatar";
 
@@ -19,24 +25,46 @@ export default function MemberSelect({
 }: MemberSelectProps) {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4 justify-center">
-      <Select
-        label={label || "主推"}
-        placeholder={name || "选择干员"}
+      <Autocomplete
+        label={label}
+        placeholder={name}
         className="w-52 "
-        inputMode="search"
-        onChange={(e) => {
-          onSave?.(e.target.value);
+        defaultItems={data || memberNameAvatarMap}
+        onSelectionChange={(val: any) => {
+          onSave?.(val);
         }}
       >
-        {(data || memberNameAvatarMap).map((item) => (
-          <SelectItem key={item.avatar} textValue={item.name}>
+        {(item) => (
+          <AutocompleteItem key={item.avatar} textValue={item.name}>
             <div className="flex items-center">
-              <LazyLoadAvatar url={item.avatar} useAvatar />
+              <LazyLoadAvatar url={item.avatar!} useAvatar />
               <span className="ml-3">{item.name}</span>
             </div>
-          </SelectItem>
-        ))}
-      </Select>
+          </AutocompleteItem>
+        )}
+      </Autocomplete>
     </div>
   );
+  // return (
+  //   <div className="flex w-full flex-wrap md:flex-nowrap gap-4 justify-center">
+  //     <Select
+  //       label={label || "主推"}
+  //       placeholder={name || "选择干员"}
+  //       className="w-52 "
+  //       inputMode="search"
+  //       onChange={(e) => {
+  //         onSave?.(e.target.value);
+  //       }}
+  //     >
+  //       {(data || memberNameAvatarMap).map((item) => (
+  //         <SelectItem key={item.avatar} textValue={item.name}>
+  //           <div className="flex items-center">
+  //             <LazyLoadAvatar url={item.avatar} useAvatar />
+  //             <span className="ml-3">{item.name}</span>
+  //           </div>
+  //         </SelectItem>
+  //       ))}
+  //     </Select>
+  //   </div>
+  // );
 }
