72260b7d9b434e6f6ad723d530ebaa68bc1940f3
SHUAXIN <shuaxinjs@qq.com>
2025-04-17 17:52:00 +0800

    初始化语言判断

--- src/i18n.ts
+++ src/i18n.ts
@@ -265,11 +265,22 @@ export const resources = {
     }
 } as const;
 
+const language = navigator.language || navigator.language;
+console.log(language); // 例如 "en-US", "zh-CN", "ja", etc.
+
+const langMap = {
+    'zh-CN': 'zh',
+    'zh-TW': 'zh',
+    'zh-HK': 'zh',
+    'zh': 'zh',
+    'ja': 'jp',
+}
+
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: "zh", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+        lng: langMap[language] || "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
         // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
         // if you're using a language detector, do not define the lng option
 
