eff662373e49c15fc161978d1902f572666711f5
SHUAXIN <shuaxinjs@qq.com>
2025-07-09 14:24:23 +0800

    新增capacitorjs

---
+++ android/app/src/main/java/arknights/shuaxinjs/cn/MainActivity.java
@@ -0,0 +1,5 @@
+package arknights.shuaxinjs.cn;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'arknights.shuaxinjs.cn',
+  appName: 'arknightsinfo',
+  webDir: 'dist'
+};
+
+export default config;
